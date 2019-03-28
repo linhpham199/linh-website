a4c8d5f2e58d4e078e660a2680410e660d01c10c
Linh Pham <linhpt199@gmail.com>
2019-03-29 00:43:15 +0200

    Complete contact form

--- src/views/ContactMe.js
+++ src/views/ContactMe.js
@@ -4,7 +4,23 @@ class ContactMe extends Component {
   render() {
     return (
       <div>
-        Get in touch.
+        <form className="form">
+          <p className="form__container">
+            <label for="name" className="form__label">Name</label>
+            <input className="form__input" type="text" name="name" id="name" required/>
+          </p>
+          <p className="form__container">
+            <label for="email" className="form__label">Email address</label>
+            <input className="form__input" type="email" name="email" id="email" required/>
+          </p>
+          <p className="form__container">
+            <label for="message" className="form__label">Leave me a message</label>
+            <textarea className="form__input" name="message" rows="5"></textarea>
+          </p>
+          <p className="form__container">
+            <button type="submit" className="form__button">Send</button>
+          </p>
+        </form>
       </div>
     );
   }
