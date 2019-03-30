5e0d2f81fbed6d6098569a37e008b79ac2fce99f
Linh Pham <linhpt199@gmail.com>
2019-03-30 20:30:32 +0200

    Connect firebase to contact form

--- src/views/ContactMe.js
+++ src/views/ContactMe.js
@@ -1,26 +1,86 @@
 import React, { Component } from 'react';
+import db  from '../firebase/firebase';
 
 class ContactMe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      shown: false,
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    console.log(this.state)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { name, email, message } = this.state
+    
+    const newMessagesRef = db.ref('messages').push()
+    newMessagesRef.set({
+      name,
+      email,
+      message
+    })
+
+    console.log(this.state.shown)
+    this.setState({
+      name: "",
+      email: "",
+      message: "",
+      shown: !this.state.shown
+    })
+
+    setTimeout(
+      function() {
+        console.log(this.state.shown)
+        this.setState({shown: !this.state.shown})
+      }.bind(this),
+      3000
+    )
+
+    
+  }
+
   render() {
+    var shown = {
+			display: this.state.shown ? "block" : "none"
+    };
+    const messageTitle = "Your message has been sent!"
+    const messageContent = "I will get back to you as soon as possible. Thank you!"
+
     return (
       <div>
         <form className="form">
           <p className="form__container">
-            <label for="name" className="form__label">Name</label>
-            <input className="form__input" type="text" name="name" id="name" required/>
+            <label htmlFor="name" className="form__label">Name</label>
+            <input className="form__input" type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} required/>
           </p>
           <p className="form__container">
-            <label for="email" className="form__label">Email address</label>
-            <input className="form__input" type="email" name="email" id="email" required/>
+            <label htmlFor="email" className="form__label">Email address</label>
+            <input className="form__input" type="email" name="email" id="email" value={this.state.emal} onChange={this.handleChange} required/>
           </p>
           <p className="form__container">
-            <label for="message" className="form__label">Leave me a message</label>
-            <textarea className="form__input" name="message" rows="5"></textarea>
+            <label htmlFor="message" className="form__label">Leave me a message</label>
+            <textarea className="form__input" name="message" rows="7" value={this.state.message} onChange={this.handleChange}></textarea>
           </p>
           <p className="form__container">
-            <button type="submit" className="form__button">Send</button>
+            <button type="submit" className="form__button" onClick={this.handleSubmit}>Send</button>
           </p>
+          <div style={ shown } className="form__message">
+            <h4>{messageTitle}</h4>
+            <p>{messageContent}</p>
+        </div>
         </form>
+        
       </div>
     );
   }
