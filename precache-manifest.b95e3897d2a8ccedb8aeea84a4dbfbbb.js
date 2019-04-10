e2945ac03c4406eeb930d5af5c7da4e07661996e
Linh Pham <linhpt199@gmail.com>
2019-04-10 16:10:30 +0300

    Updates

---
+++ precache-manifest.b95e3897d2a8ccedb8aeea84a4dbfbbb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "704cb45a166338d3a9624082832005c2",
+    "url": "/static/media/background-side.704cb45a.jpg"
+  },
+  {
+    "revision": "71ec65abe8133906e779",
+    "url": "/static/css/main.c57d7bbb.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "63a9d4dec347085e3151",
+    "url": "/static/js/2.5100d660.chunk.js"
+  },
+  {
+    "revision": "44294c4a5a9257730c0dfcd249eeabbe",
+    "url": "/static/media/pg-card.44294c4a.jpg"
+  },
+  {
+    "revision": "e164d1a1f95223f1fb5893e96ca5da8c",
+    "url": "/static/media/food-card.e164d1a1.jpg"
+  },
+  {
+    "revision": "71ec65abe8133906e779",
+    "url": "/static/js/main.8b370100.chunk.js"
+  },
+  {
+    "revision": "067d813bc72c1f5b6cf3fdf2aba0d969",
+    "url": "/static/media/tc-card.067d813b.jpg"
+  },
+  {
+    "revision": "b4820588344d738f903ce6bd315be1d9",
+    "url": "/static/media/fork-card.b4820588.jpg"
+  },
+  {
+    "revision": "41a175b7f8d147bdf44b047f0d7e9de6",
+    "url": "/static/media/wpm-card.41a175b7.jpg"
+  },
+  {
+    "revision": "fa2497685ac887ef825b3ea0dc4ad81f",
+    "url": "/static/media/pb-card.fa249768.jpg"
+  },
+  {
+    "revision": "a2562d6a5eaede74a9fbed9e76178903",
+    "url": "/index.html"
+  }
+];
